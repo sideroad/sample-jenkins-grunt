3d2c01f755d90aa9ed01915da742df45cf0ea430
sideroad <sideroad.jp@gmail.com>
2013-04-13 15:14:33 +0900

    Append path

--- jenkins-grunt.sh
+++ jenkins-grunt.sh
@@ -1,4 +1,4 @@
-export PATH=/usr/local/bin:/path/to/node:/path/to/node_bin:/path/to/phantomjs:$PATH;
+export PATH=/usr/local/bin:/path/to/node:/path/to/node_bin:/path/to/phantomjs:/path/to/jscoverage:$PATH;
 
 mkdir node_modules
 cp -R /path/to/grunt_node_modules/* node_modules
