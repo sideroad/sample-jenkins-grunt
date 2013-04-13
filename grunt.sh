f02321490dd16c1515bb411823e5758d50d70343
sideroad <sideroad.jp@gmail.com>
2013-04-13 12:24:45 +0900

    Debug

--- grunt.sh
+++ grunt.sh
@@ -1,5 +1,6 @@
 export PATH=/path/to/node:/path/to/node_bin:/path/to/node_lib:$PATH;
 
 cp -R /path/to/grunt_node_modules node_modules
+cp -R /path/to/global_node_modules node_modules
 ls -la
 grunt
