1b7a9bb5eb2916ccffdf4c90ad87dd8242f83c76
sideroad <sideroad.jp@gmail.com>
2013-06-26 23:41:48 +0900

    Change approach to install node module

--- jenkins-grunt.sh
+++ jenkins-grunt.sh
@@ -1,5 +1,5 @@
 export PATH=/usr/local/bin:/path/to/node:/path/to/node_bin:/path/to/phantomjs:/path/to/jscoverage:$PATH;
 
 mkdir node_modules
-cp -R /path/to/grunt_node_modules/* node_modules
+npm install
 grunt jenkins --no-color
